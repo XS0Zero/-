3d49f17cac5ed87191e6ac9fad899502c2e2223f
XS0Zero <1287579544@qq.com>
2024-06-19 01:21:55 +0800

    登录界面通过

---
+++ ctrl/Login_slot.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import pandas as pd
+
+array = []
+def getUserInfo():
+    print("当前工作目录：%s" % os.getcwd())
+    data = pd.read_csv(r'user.csv', sep=',', header=0)
+    global array
+    array = data.values[0::, 0::]  # 读取全部行，全部列
+    print(array)
+
+
+def on_login_button_clicked(self,window,Main_window):
+    print("登录按钮被点击了")
+    # 在这里添加登录逻辑
+    if check_password(self.lineEdit.text(), self.lineEdit_2.text()):
+        print("登录成功")
+        window.hide()
+        Main_window.show()
+    else:
+        print("登录失败")
+
+
+def on_exit_button_clicked():
+    print("退出按钮被点击了")
+    # 在这里添加退出逻辑，例如关闭窗口或退出程序
+    sys.exit()
+
+
+def get_input_text(username, password):
+    # username = username.text()
+    # password = password.text()
+    print("用户名:", username, "密码:", password)
+    return check_password(username, password)
+
+def check_password(n, p):
+    for i in range(len(array)):
+        if array[i][0] == n and array[i][1] == p:
+            return True
+    return False
